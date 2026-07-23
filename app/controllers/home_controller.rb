652c11a6ae66ab6fba3a47b4472c1a7ec044a62d
Billy Gonzalez <william.gonzalez1@uga.edu>
2026-07-23 13:23:21 -0400

    Basic management page created: no utility yet. Notes: need to add the tests and a way to create a test db.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,32 @@
 class HomeController < ApplicationController
   def index
+    @Users = User.all
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to rot_path, notice: "User created."
+    else
+      redirect_to root_path, alert: "Unable to create user."
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to root_path, notice: "User updated."
+    else
+      redirect_to root_path, alert: "Unable to update user."
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    redirect_to root_path, notice: "User deleted."
   end
 end
